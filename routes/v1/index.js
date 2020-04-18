2cae065fb18ad7f33314b399a2b0e13189a3147e
bim240 <bimlendu357@gmail.com>
2020-04-18 20:52:27 +0530

    route defined for adding furniture

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var usersRouter = require("./users");
+var itemsRouteer = require("./items");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,4 +9,5 @@ router.get("/", function (req, res, next) {
 });
 
 router.use("/users", usersRouter);
+router.use("/items", itemsRouteer);
 module.exports = router;
---
+++ routes/v1/items.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+var itemController = require("../../controller/v1/items");
+
+// router.get("/", itemController.getAllItems);
+router.use("/:name", itemController.addItems);
+
+module.exports = router;
